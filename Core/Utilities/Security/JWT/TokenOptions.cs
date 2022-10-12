b0546907b1fbe9e7cf2d12159c1e3fc12577140c
aktepeayse96 <aktepeayse96@gmail.com>
2022-10-12 14:37:13 +0300

    backend son

--- Core/Utilities/Security/JWT/TokenOptions.cs
+++ Core/Utilities/Security/JWT/TokenOptions.cs
@@ -8,9 +8,10 @@ namespace Core.Utilities.Security.JWT
 {
     public class TokenOptions
     {
-        public string Audience { get; set; }
-        public string Issuer { get; set; }
+        public string? Audience { get; set; }
+        public string? Issuer { get; set; }
         public int AccessTokenExpiration { get; set; }
-        public string SecurityKey { get; set; }
+        public string? SecurityKey { get; set; }
+
     }
 }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,38 +3,57 @@ using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
 using Business.DependencyResolvers.Autofac;
+using Core.DependencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = tokenOptions.Issuer,
+            ValidAudience = tokenOptions.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+        };
+    });
+
+//IServiceCollectionExtension;
+builder.Services.AddDependencyResolvers(new ICoreModule[] {
+    new CoreModule() });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//araba için
-//builder.Services.AddSingleton<ICarService, CarManager>();
-//builder.Services.AddSingleton<ICarDal, EfCarDal>();
-////marka için
-//builder.Services.AddSingleton<IBrandService, BrandManager>();
-//builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
-////color
-//builder.Services.AddSingleton<IColorService, ColorManager>();
-//builder.Services.AddSingleton<IColorDal, EfColorDal>();
-////user
-//builder.Services.AddSingleton<IUserService, UserManager>();
-//builder.Services.AddSingleton<IUserDal, EfUserDal>();
-////customer
-//builder.Services.AddSingleton<ICustomerService, CustomerManager>();
-//builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
-////Rental
-//builder.Services.AddSingleton<IRentalService, RentalManager>();
-//builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
+
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
+
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
+//----------------------------------
+
 
 var app = builder.Build();
 
@@ -42,13 +61,21 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
+app.UseRouting();
+
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
